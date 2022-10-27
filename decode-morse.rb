f9ef7db3594710d1198120748a451d58294d239a
John <kibirigejohn005@gmail.com>
2022-10-27 18:38:02 +0300

    Fix linter errors

--- decode-morse.rb
+++ decode-morse.rb
@@ -1,57 +1,57 @@
-def decode_morse_code (m_code) 
-    return split_morse_sentence(m_code).map{|word| 
-        decode_single_word(word)
-    }.join(' ')
+def decode_morse_code(m_code)
+  return split_morse_sentence(m_code).map { |word|
+    decode_single_word(word)
+  }.join(' ')
 end
 
-def split_morse_sentence (morse_sentence)
-    return morse_sentence.split("   ")
+def split_morse_sentence(morse_sentence)
+  return morse_sentence.split("   ")
 end
 
 def decode_single_word(word)
-   return word.split(' ').map{|char| decode_char(char)}.join('')
+  return word.split(' ').map { |char| decode_char(char) }.join('')
 end
 
-def decode_char (character) 
-    repo = {
-        ".-" => "A",
-        "-..." => "B",
-        "-.-." => "C",
-        "-.." => "D",
-        "." => "E",
-        "..-." => "F",
-        "--." => "G",
-        "...." => "H",
-        ".." => "I",
-        ".---" => "J",
-        "-.-" => "K",
-        ".-.." => "L",
-        "--" => "M",
-        "-." => "N",
-        "---" => "O",
-        ".--." => "P",
-        "--.-" => "Q",
-        ".-." => "R",
-        "..." => "S",
-        "-" => "T",
-        "..-" => "U",
-        "...-" => "V",
-        ".--" => "W",
-        "-..-" => "X",
-        "-.--" => "Y",
-        "--.." => "Z",
-        ".----" => "1",
-        "..---" => "2",
-        "...--" => "3",
-        "....-" => "4",
-        "....." => "5",
-        "-...." => "6",
-        "--..." => "7",
-        "---.." => "8",
-        "----." => "9",
-        "-----" => "0"
-    }
-    return repo[character]
+def decode_char(character)
+  repo = {
+    ".-" => "A",
+    "-..." => "B",
+    "-.-." => "C",
+    "-.." => "D",
+    "." => "E",
+    "..-." => "F",
+    "--." => "G",
+    "...." => "H",
+    ".." => "I",
+    ".---" => "J",
+    "-.-" => "K",
+    ".-.." => "L",
+    "--" => "M",
+    "-." => "N",
+    "---" => "O",
+    ".--." => "P",
+    "--.-" => "Q",
+    ".-." => "R",
+    "..." => "S",
+    "-" => "T",
+    "..-" => "U",
+    "...-" => "V",
+    ".--" => "W",
+    "-..-" => "X",
+    "-.--" => "Y",
+    "--.." => "Z",
+    ".----" => "1",
+    "..---" => "2",
+    "...--" => "3",
+    "....-" => "4",
+    "....." => "5",
+    "-...." => "6",
+    "--..." => "7",
+    "---.." => "8",
+    "----." => "9",
+    "-----" => "0"
+  }
+  return repo[character]
 end
 
 print decode_morse_code('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
